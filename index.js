0c37cdbeaa612b0d7a75d1a04dbd2416f807bea3
uday kumar <udaykumar.ab2017@gmail.com>
2022-03-22 13:51:31 +0530

    bugs1

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ const server = app.listen(PORT, () => {
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "*",
+    origin: "https://uksocialfinal.herokuapp.com/",
   },
 });
 
